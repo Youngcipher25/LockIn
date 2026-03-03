4b891ab4d27df81415bdd52ea8ee5652fdeeedf1
Murali <murali@progressive-solution.com>
2026-03-03 20:32:36 +0530

    Initial Commit

---
+++ LockIn/LockInApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockInApp.swift
+//  LockIn
+//
+//  Created by Admin on 03/03/26.
+//
+
+import SwiftUI
+
+@main
+struct LockInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
